2cf7e71e0ef42eb5bdcddee5b3d780ae6323d233
Muhammad Ahmad <100872369+MuhammadAhmad33@users.noreply.github.com>
2024-12-13 02:10:46 +0500

    Added: user page

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../modules/auth/service/auth/auth.service';
 
-
 @Injectable({
   providedIn: 'root'
 })
